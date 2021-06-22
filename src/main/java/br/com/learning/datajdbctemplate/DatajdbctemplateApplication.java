c1e4389b388d584edf2122dc8137dd6f2dc5c195
jordihofc <jordi.silva@ufv.br>
2021-06-22 11:03:54 -0300

    create setup

---
+++ src/main/java/br/com/learning/datajdbctemplate/DatajdbctemplateApplication.java
@@ -0,0 +1,13 @@
+package br.com.learning.datajdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatajdbctemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatajdbctemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/learning/datajdbctemplate/DatajdbctemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.learning.datajdbctemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatajdbctemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
